b58e95b057e1ce9b931db93f1da9dd7097045b83
Simon Mei <smei8800@gmail.com>
2019-08-08 00:07:34 -0400

    graphql is pretty damn amazing

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 const port = 3000
 
 app.use("/graphql", graphqlHTTP({
-  
+  schema,
+  graphiql: true
 }));
 
 app.listen(port, () => {
